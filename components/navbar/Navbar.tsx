6396efe21119a7edc3271805871fed853fe2d05d
Arup53 <arupkumarhowlader12@gmail.com>
2025-03-20 09:34:02 +0600

    modify navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -5,22 +5,22 @@ import { Button } from "../ui/button";
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center px-6 py-4 bg-white shadow-md border-b-2">
+    <nav className=" flex justify-center items-center px-6 py-4 bg-black ">
       {/* Left Side - Brand Name */}
-      <div className="text-2xl font-bold text-gray-900">FinAI</div>
+      {/* <div className="text-2xl font-bold text-white">FinAI</div> */}
 
       {/* Right Side - Buttons */}
-      <div className="flex gap-4">
-        <Button className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700 transition">
+      <div className="flex gap-4  px-4 py-2">
+        <Button className="px-4 py-2 bg-white text-black rounded-full hover:bg-blue-700 transition">
+          Log In
+        </Button>
+        <Button className="px-4 py-2 bg-white text-black rounded-full hover:bg-blue-700 transition">
           <Link href="/dashboard">Dashboard</Link>
         </Button>
 
         {/* <button className="px-4 py-2 text-white border rounded-lg bg-gradient-to-r from-pink-300 to-purple-300  ">
           AI Chat
         </button> */}
-        <Button className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700 transition">
-          Log In
-        </Button>
       </div>
     </nav>
   );
