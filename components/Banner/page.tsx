aca84987b987f9d46cf00eaa0f54966e8cdee207
Arup53 <arupkumarhowlader12@gmail.com>
2025-03-20 09:39:21 +0600

    fix reference error for lottie react

--- components/Banner/page.tsx
+++ components/Banner/page.tsx
@@ -1,15 +1,14 @@
 "use client";
-import Lottie from "lottie-react";
 
+import dynamic from "next/dynamic";
 import bannerAnimation from "../animation/bannerAnimation.json";
 
-// const handleGetStarted = () => {
-//   console.log("Button clicked");
-// };
+// Import Lottie dynamically with SSR disabled
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 
 export default function Banner() {
   return (
-    <section className="flex  justify-between items-center text-center min-h-screen bg-black px-48">
+    <section className="flex justify-between items-center text-center min-h-screen bg-black px-48">
       <div className="flex flex-col justify-center items-center">
         <h1 className="text-5xl font-bold text-white mt-4">FinAI</h1>
         <h3 className="text-5xl font-bold text-white mt-4">
@@ -17,11 +16,6 @@ export default function Banner() {
           with AI
         </h3>
         <p className="mt-4 text-white">AI AGENT For Crypto Investment</p>
-        {/* <Button
-          text="GET STARTED"
-          style="mt-6 px-6 py-3 text-white bg-blue-500 rounded-lg hover:bg-blue-600 transition"
-          handler={handleGetStarted}
-        /> */}
       </div>
 
       <div>
